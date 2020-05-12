8c1cb15a54046eddc5b2fae4525626fab77b08da
zhanghao111000 <1552556230@qq.com>
2020-05-12 16:05:46 +0800

    组员李创建test01方法

--- src/main/java/cn/com/ssm/test/Demo.java
+++ src/main/java/cn/com/ssm/test/Demo.java
@@ -4,4 +4,9 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("创建main方法");
     }
+
+    public void test01(){
+        System.out.println("组员李创建test01方法");
+    }
+
 }
